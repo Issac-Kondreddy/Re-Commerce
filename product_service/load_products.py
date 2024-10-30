c6b5afdd0a8efa679b16b7c310fdd01730469f67
issac-kondreddy <ixk93660@ucmo.edu>
2024-10-30 11:16:44 -0500

    Initial setup for Product Service

---
+++ product_service/load_products.py
@@ -0,0 +1,23 @@
+import csv
+import os
+import django
+from .models import Product  # Replace 'myproductapp' with your app name
+
+# Set up Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "product_service.settings") 
+django.setup()
+
+def load_products_from_csv(file_path):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            Product.objects.create(
+                name=row['name'],
+                description=row['description'],
+                price=float(row['price']),
+                stock=int(row['stock'])
+            )
+    print("Products loaded successfully!")
+
+# Run the function with the path to products.csv
+load_products_from_csv('products.csv')
